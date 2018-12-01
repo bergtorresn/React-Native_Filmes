cc16fb40f923e5bf27a900990d9978773db21ab5
Rosemberg Torres Nunes <bergtorres1@gmail.com>
2018-12-01 14:49:47 -0300

    Listando dados local

--- screens/ListaDeFilmesScreen.js
+++ screens/ListaDeFilmesScreen.js
@@ -1,23 +1,38 @@
-import React, {Component} from 'react';
-import {Text, View, StyleSheet, Image, Dimensions} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Image, Dimensions, FlatList } from 'react-native';
 
 const larguraDaTela = Dimensions.get('screen').width;
 
+/*
+Observações:
+
+Para utlizar código js dentro da sintaxe JSX, deve implementar o código dentro de chaves {}
+*/
 export default class ListaDeFilmesScreen extends Component {
   render() {
+    const filmes = [{ id: 1, usuario: 'teste1' },
+    { id: 2, usuario: 'teste2' },
+    { id: 3, usuario: 'teste3' },
+    { id: 4, usuario: 'teste4' }];
     return (
-      <View>
-        <Image source={require('../resources/placeholder/placeholder_filme.png')} style={styles.imgFilme}/>
-      </View>
+      <FlatList data={filmes} renderItem={({ item }) =>
+        <View style={styles.row}>
+          <Text>{item.usuario}</Text>
+          <Image source={require('../resources/placeholder/placeholder_filme.png')} style={styles.imgFilme} />
+        </View>
+      } keyExtractor={item => item.id.toString()}
+      />
     );
   }
 }
-// Utilizando imagem local
-// <Image source={require('diretorio')}/> como require é uma func js tem que chamar ela dentro de {}
+
 
 const styles = StyleSheet.create({
   imgFilme: {
     width: larguraDaTela,
     height: larguraDaTela
+  },
+  row: {
+    marginTop: 30
   }
 });
