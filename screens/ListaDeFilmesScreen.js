a40bddfc338909cc3dbcbc2baba239c240ddac28
Rosemberg Torres Nunes <bergtorres1@gmail.com>
2018-12-01 16:07:15 -0300

    filmes dentro de state

--- screens/ListaDeFilmesScreen.js
+++ screens/ListaDeFilmesScreen.js
@@ -4,15 +4,21 @@ import {
 } from 'react-native';
 import Filme from '../components/Filme';
     
-const filmes = [{ id: 1, usuario: 'teste1' },
-{ id: 2, usuario: 'teste2' },
-{ id: 3, usuario: 'teste3' },
-{ id: 4, usuario: 'teste4' }];
-
 export default class ListaDeFilmesScreen extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      filmes: [{ id: 1, usuario: 'teste1' },
+      { id: 2, usuario: 'teste2' },
+      { id: 3, usuario: 'teste3' },
+      { id: 4, usuario: 'teste4' }]
+    }
+  }
+
   render() {
     return (
-      <FlatList data={filmes} 
+      <FlatList data={this.state.filmes} 
       keyExtractor={item => item.id.toString()}
       renderItem={({ item }) =>
         <Filme filme={item}/>
