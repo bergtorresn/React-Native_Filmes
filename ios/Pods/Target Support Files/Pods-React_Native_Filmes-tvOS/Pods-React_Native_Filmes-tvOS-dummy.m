3c49fe11af20629ddea6263fb29896fc22548d68
Rosemberg Torres Nunes <bergtorres1@gmail.com>
2018-12-03 22:21:53 -0300

    configurando firebase no projeto ios e android

---
+++ ios/Pods/Target Support Files/Pods-React_Native_Filmes-tvOS/Pods-React_Native_Filmes-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_React_Native_Filmes_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_React_Native_Filmes_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-React_Native_Filmes/Pods-React_Native_Filmes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_React_Native_Filmes : NSObject
+@end
+@implementation PodsDummy_Pods_React_Native_Filmes
+@end
