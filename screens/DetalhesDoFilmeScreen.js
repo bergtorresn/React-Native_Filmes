4fd555a752e82a6df73171be23315903e685a1f8
Rosemberg Torres Nunes <bergtorres1@gmail.com>
2018-12-06 22:00:38 -0300

    AsyncStorage

--- screens/DetalhesDoFilmeScreen.js
+++ screens/DetalhesDoFilmeScreen.js
@@ -7,7 +7,8 @@ import {
   Image,
   ScrollView,
   TouchableOpacity,
-  Alert
+  Alert,
+  AsyncStorage
 } from 'react-native';
 
 const larguraDaTela = Dimensions.get('screen').width;
@@ -23,13 +24,35 @@ export default class DetalhesDoFilmeScreen extends Component {
       <ScrollView>
         <View style={styles.filmeDetalheView}>
           <Image source={{ uri: "https://image.tmdb.org/t/p/w500/VuukZLgaCrho2Ar8Scl9HtV3yD.jpg" }} style={styles.filmeDetalheCapa} />
-          <TouchableOpacity onPress={() => Alert.alert("Like e Deslike")} underlayColor='black' style={styles.filmeDetalheBtnFavoritar}>
+          <TouchableOpacity onPress={this.marcarFilme} underlayColor='black' style={styles.filmeDetalheBtnFavoritar}>
+            <Text>Salvar</Text>
           </TouchableOpacity>
           <Text style={styles.filmeDetalheTitulo}>Animais Fantásticos: Os Crimes de Grindelwald</Text>
           <Text style={styles.filmeDetalheSinopse}>Comédia de artes marciais seguindo um grupo de lendas do kung fu se unindo para enfrentar os bandidos. As lendas incluem VINCENT ZHAO reprisando seu papel como "Wong Fei Hung" com DENNIS TO retratando o mestre do "Wing Chun" "Ip Man", DANNY CHAN KWOK KWAN como "Chen Zhen" e ANDY ON como mestre "Huo Yuan Jia".</Text>
         </View>
       </ScrollView>
     );
+  }
+}
+
+marcarFilme = async () => {
+  try {
+    await AsyncStorage.setItem('testando', 'teste');
+    Alert.alert("armazenou");
+  } catch (error) {
+    Alert.alert(error);
+  }
+}
+
+desmarcarFilme = async () => {
+  try {
+    const value = await AsyncStorage.getItem('testando');
+    Alert.alert("nulo");
+    if (value !== null) {
+      Alert.alert(value);
+    }
+  } catch (error) {
+    Alert.alert(error);
   }
 }
 
