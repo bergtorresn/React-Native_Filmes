6fb9485f7b13c5f810c6d079065de5e57b80a1ad
Rosemberg Torres Nunes <bergtorres1@gmail.com>
2018-12-06 21:38:36 -0300

    Criando botão de like e deslike

--- screens/DetalhesDoFilmeScreen.js
+++ screens/DetalhesDoFilmeScreen.js
@@ -5,7 +5,9 @@ import {
   Text,
   Dimensions,
   Image,
-  ScrollView
+  ScrollView,
+  TouchableOpacity,
+  Alert
 } from 'react-native';
 
 const larguraDaTela = Dimensions.get('screen').width;
@@ -19,14 +21,14 @@ export default class DetalhesDoFilmeScreen extends Component {
   render() {
     return (
       <ScrollView>
-
         <View style={styles.filmeDetalheView}>
           <Image source={{ uri: "https://image.tmdb.org/t/p/w500/VuukZLgaCrho2Ar8Scl9HtV3yD.jpg" }} style={styles.filmeDetalheCapa} />
+          <TouchableOpacity onPress={() => Alert.alert("Like e Deslike")} underlayColor='black' style={styles.filmeDetalheBtnFavoritar}>
+          </TouchableOpacity>
           <Text style={styles.filmeDetalheTitulo}>Animais Fantásticos: Os Crimes de Grindelwald</Text>
           <Text style={styles.filmeDetalheSinopse}>Comédia de artes marciais seguindo um grupo de lendas do kung fu se unindo para enfrentar os bandidos. As lendas incluem VINCENT ZHAO reprisando seu papel como "Wong Fei Hung" com DENNIS TO retratando o mestre do "Wing Chun" "Ip Man", DANNY CHAN KWOK KWAN como "Chen Zhen" e ANDY ON como mestre "Huo Yuan Jia".</Text>
         </View>
       </ScrollView>
-
     );
   }
 }
@@ -50,6 +52,16 @@ const styles = StyleSheet.create({
     marginTop: 10,
     width: larguraDaTela,
     fontSize: 18,
+  },
+  filmeDetalheBtnFavoritar: {
+    width: 50,
+    height: 50,
+    alignSelf: 'flex-end',
+    backgroundColor: 'red',
+    marginRight: 5,
+    marginTop: 5,
+    marginBottom: 5
   }
+
 });
 
